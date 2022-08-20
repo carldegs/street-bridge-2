368ec2ec0437c15b9d2a24d05d1e9875da241840
Carl de Guia <carl.2795@gmail.com>
2022-08-20 10:41:42 +0800

    Fix font not loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 
 import { PlayProvider } from '../hooks/PlayContext';
 import theme from '../theme';
+import Fonts from '../theme/Fonts';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <PlayProvider>
       <ChakraProvider theme={theme}>
+        <Fonts />
         <Component {...pageProps} />
       </ChakraProvider>
     </PlayProvider>
